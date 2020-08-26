e4083065ec82ec6c6e819d0d61a1849318422a28
Benjamin karami <51356198+benjamin-karami@users.noreply.github.com>
2020-08-26 16:53:24 +0430

    Add files via upload

---
+++ precache-manifest.4be34a9f6b69ba7b86b3725defe8e949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17195382fb1723650ffbd215a60ca8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58ac1f31b11ffbdb1c64",
+    "url": "/static/css/main.106cab20.chunk.css"
+  },
+  {
+    "revision": "74ff0d49b607a7af9afa",
+    "url": "/static/js/2.7a087d06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7a087d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ac1f31b11ffbdb1c64",
+    "url": "/static/js/main.79f06df5.chunk.js"
+  },
+  {
+    "revision": "fa0f15b8e6d9c7933b51",
+    "url": "/static/js/runtime-main.f4c23f94.js"
+  }
+]);
